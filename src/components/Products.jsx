85de6641187b4b877f8be085a8db035272a7d630
rohan <ronnie2255@icloud.com>
2021-12-26 16:21:26 -0500

    Popular Products

---
+++ src/components/Products.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import {popularProducts} from "../data";
+
+const Container = styled.div``;
+
+export const Products = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
