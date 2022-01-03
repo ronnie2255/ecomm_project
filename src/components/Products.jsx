2afd4dc1719210dd4b791097ad3280f58d282ed9
rohan <ronnie2255@icloud.com>
2022-01-03 13:54:23 -0500

    product and products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 import {popularProducts} from "../data";
 
-const Container = styled.div``;
+const Container = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-wrap: wrap;
+`;
 
 export const Products = () => {
     return (
         <Container>
-            
+            {popularProducts.map((item)=> (
+                <Products item={item} key={item.id} />
+            ))}
         </Container>
     )
 }
+
+export default Products
