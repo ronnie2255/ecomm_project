e8892e6b0c21009eee92cedf1e639ef8395d951f
rohan <ronnie2255@icloud.com>
2022-01-03 18:37:43 -0500

    Single Product page

--- src/data.js
+++ src/data.js
@@ -68,7 +68,7 @@ export const sliderItems = [
     },
     {
       id:7,
-      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
     },
     {
       id:8,
