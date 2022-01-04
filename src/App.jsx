7c7ddcdfe09b296b98edd1d534abd557c9c47461
rohan <ronnie2255@icloud.com>
2022-01-04 14:59:22 -0500

    Login and Register completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Home from "./pages/Home";
-import Product from "./pages/Product";
-import ProductList from "./pages/ProductList";
+//import Home from "./pages/Home";
+//import Product from "./pages/Product";
+//import ProductList from "./pages/ProductList";
+import Login from "./pages/Login";
+//import Register from "./pages/Register";
 
 
 const App = () => {
-  return <Product/>;
+  return <Login/>;
 };
 
 export default App;
